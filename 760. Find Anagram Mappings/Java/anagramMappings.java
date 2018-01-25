2f62ed74a99a449815f5ae7d6a4790cd6e3d0c87
Yubo Zhang <clannad93@gmail.com>
2018-01-25 12:43:25 -0800

    760. Find Anagram Mappings

---
+++ 760. Find Anagram Mappings/Java/anagramMappings.java
@@ -0,0 +1,25 @@
+import java.util.Arrays;
+
+public class anagramMappings {
+    public int[] anagramMappings(int[] A, int[] B) {
+        int size = A.length;
+        int[] result = new int[size];
+        for (int i =0;i<size;i++){
+            for (int j =0;j<size;j++){
+                if(A[i]==B[j]){
+                    result[i]=j;
+                }
+            }
+        }
+        return result;
+    }
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		anagramMappings newclass = new anagramMappings();
+		int[] A = {12, 28, 46, 32, 50};
+		int[] B = {50, 12, 32, 46, 28};
+		System.out.println(Arrays.toString(newclass.anagramMappings(A,B)));
+
+	}
+
+}
