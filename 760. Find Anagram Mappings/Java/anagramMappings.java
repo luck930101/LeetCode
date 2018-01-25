a0ea539619496ca33681cdb04b9388e6c3c154ad
Yubo Zhang <clannad93@gmail.com>
2018-01-25 12:45:23 -0800

    rename

--- 760. Find Anagram Mappings/Java/anagramMappings.java
+++ 760. Find Anagram Mappings/Java/anagramMappings.java
@@ -1,6 +1,6 @@
 import java.util.Arrays;
 
-public class anagramMappings {
+public class AnagramMappings {
     public int[] anagramMappings(int[] A, int[] B) {
         int size = A.length;
         int[] result = new int[size];
@@ -15,7 +15,7 @@ public class anagramMappings {
     }
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		anagramMappings newclass = new anagramMappings();
+		AnagramMappings newclass = new AnagramMappings();
 		int[] A = {12, 28, 46, 32, 50};
 		int[] B = {50, 12, 32, 46, 28};
 		System.out.println(Arrays.toString(newclass.anagramMappings(A,B)));
