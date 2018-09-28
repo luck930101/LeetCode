8188b2962ce563e0345e1c3c8aa64b3ac7adac1b
Yubo Zhang <clannad93@gmail.com>
2018-09-28 15:23:23 -0700

    709. To Lower Case
    709. To Lower Case    Super easy

---
+++ 709. To Lower Case    /Java/ToLowerCase.java
@@ -0,0 +1,15 @@
+
+public class ToLowerCase {
+	
+    public String toLowerCase(String str) {
+        return str.toLowerCase();
+    }
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ToLowerCase tlc = new ToLowerCase();
+		System.out.println(tlc.toLowerCase("Hello"));
+
+	}
+
+}
