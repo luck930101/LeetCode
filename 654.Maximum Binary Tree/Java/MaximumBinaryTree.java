fa0876df1337c602358138c44493994bd0a47e0b
Yubo Zhang <clannad93@gmail.com>
2018-10-03 00:16:34 -0700

    654 Maximum Binary Tree

---
+++ 654.Maximum Binary Tree/Java/MaximumBinaryTree.java
@@ -0,0 +1,19 @@
+
+public class MaximumBinaryTree {
+	
+    public TreeNode constructMaximumBinaryTree(int[] nums) {
+    	
+    	TreeNode t = null;
+        return t;
+    }
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		MaximumBinaryTree mbt = new MaximumBinaryTree();
+		
+		int[] nums = {3,2,1,6,0,5};
+		mbt.constructMaximumBinaryTree(nums);
+
+	}
+
+}
