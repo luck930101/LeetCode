ecba41a6e72f996e119df54b35fa68af1761935c
Yubo Zhang <clannad93@gmail.com>
2018-10-19 12:30:51 -0700

    Update 669. Trim a Binary Search Tree/Java/TrimaBinarySearchTree.java

---
+++ 669. Trim a Binary Search Tree/Java/TrimaBinarySearchTree.java
@@ -0,0 +1,27 @@
+
+public class TrimaBinarySearchTree {
+	public TreeNode trimBST(TreeNode root, int L, int R) {
+		if (root==null){
+			return null;
+		}
+		if (root.val >= L && root.val<= R){
+			root.left = trimBST(root.left,L,R);
+			root.right = trimBST(root.right,L,R);
+			return root;
+		}
+		else if(root.val<L){
+        	return trimBST(root.right,L,R);
+        }
+		else if(root.val>R){
+			
+        	return trimBST(root.left,L,R);
+        }
+        return root;
+    }
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
