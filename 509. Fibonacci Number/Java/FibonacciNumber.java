e61c876cabfa3b40a4dd514004c18651e30c3db6
Yubo Zhang <clannad93@gmail.com>
2019-04-13 15:48:39 -0700

    2 ways

--- 509. Fibonacci Number/Java/FibonacciNumber.java
+++ 509. Fibonacci Number/Java/FibonacciNumber.java
@@ -1,6 +1,6 @@
 
 public class FibonacciNumber {
-	public int fib(int N) {
+	public int fibRecursive(int N) {
         if(N ==0){
         	return 0;
         }
@@ -8,9 +8,28 @@ public class FibonacciNumber {
         	return 1;
         }
         
-        return fib(N-1)+fib(N-2);
+        return fibRecursive(N-1)+fibRecursive(N-2);
         
     }
+	
+	public int fibIterative(int N) {
+		if(N == 0||N == 1){
+			return N;
+		}
+
+		int pre = 0;
+		int mid = 1;
+		int post = 1;
+		
+		for(int i = 0; i< N-2;i++){
+			pre = mid;
+			mid = post;
+			post = pre+mid;
+		}
+        return post;
+    }
+	
+	
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
