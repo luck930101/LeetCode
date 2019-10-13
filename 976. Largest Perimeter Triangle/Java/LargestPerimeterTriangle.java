738385321e640a20450e0ac567d101c2de7e09fd
Yubo Zhang <clannad93@gmail.com>
2019-10-13 11:53:26 -0400

    976. Largest Perimeter Triangle

---
+++ 976. Largest Perimeter Triangle/Java/LargestPerimeterTriangle.java
@@ -0,0 +1,18 @@
+
+public class LargestPerimeterTriangle {
+	public int largestPerimeter(int[] A) {
+		
+		for(int i = A.length-1;i>1;i--){
+			if(A[i-2]>A[i]-A[i-1]){
+				return A[i]+A[i-1]+A[i-2];
+			}
+		}
+        return 0;
+    }
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
