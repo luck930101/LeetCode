92fdbdea470dc5989eea0bb417a8cdcb4111a91b
Yubo Zhang <clannad93@gmail.com>
2019-10-13 11:54:01 -0400

    Update LargestPerimeterTriangle.java

--- 976. Largest Perimeter Triangle/Java/LargestPerimeterTriangle.java
+++ 976. Largest Perimeter Triangle/Java/LargestPerimeterTriangle.java
@@ -1,7 +1,8 @@
+import java.util.Arrays;
 
 public class LargestPerimeterTriangle {
 	public int largestPerimeter(int[] A) {
-		
+		Arrays.sort(A);
 		for(int i = A.length-1;i>1;i--){
 			if(A[i-2]>A[i]-A[i-1]){
 				return A[i]+A[i-1]+A[i-2];
