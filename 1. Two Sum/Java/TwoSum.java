554c46c532fc640163edda7701e3851809efe4eb
Yubo Zhang <clannad93@gmail.com>
2019-10-26 01:01:34 -0400

    1. Two Sum update

--- 1. Two Sum/Java/TwoSum.java
+++ 1. Two Sum/Java/TwoSum.java
@@ -1,7 +1,26 @@
 import java.util.Arrays;
+import java.util.HashMap;
 
 public class TwoSum {
     public int[] twoSum(int[] nums, int target) {
+    	int[] result = new int[2];
+    	HashMap<Integer, Integer> hashmap = new HashMap<Integer, Integer>();
+    	
+    	for(int i = 0;i<nums.length;i++ ){
+    		if(hashmap.containsKey(target-nums[i])){
+    			result[0] = i;
+    			result[1] = hashmap.get(target-nums[i]);
+    			break;
+    		}
+    		else{
+    			hashmap.put(nums[i], i);
+    		}
+    	}
+    	
+        return result;
+    }
+    
+    public int[] twoSumB(int[] nums, int target) {
     	int[] result ={0,0};
         for(int i = 0; i < nums.length;i++)
         {
