23824928b3a2757514da4378fc615e85b72d095d
Yubo Zhang <clannad93@gmail.com>
2019-11-08 21:50:51 -0500

    Create SearchinRotatedSortedArray.java

---
+++ 33. Search in Rotated Sorted Array/Java/SearchinRotatedSortedArray.java
@@ -0,0 +1,34 @@
+
+public class SearchinRotatedSortedArray {
+	public int search(int[] nums, int target) {
+		int start = 0;
+		int end = nums.length-1;
+		int mid = (start + end)/2;
+		
+		while(end>=start){
+			if(target == nums[mid]){
+				return mid;
+			}
+			else if(nums[start]<=nums[mid]){
+				//First half is unrotated
+				if(target<nums[mid]&&target>=nums[start]){
+					end = mid-1;
+				}
+				else{
+					start = mid+1;
+				}
+			}
+			else{
+				//First half is rotated
+				if(target<=nums[end]&&target>nums[mid]){
+					start = mid+1;
+				}
+				else{
+					end = mid-1;
+				}
+			}
+			mid = (start + end)/2;
+		}
+        return -1;
+    }
+}
