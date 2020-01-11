b1bb1ef3c9e457aadb7e93ac7ca6bc1c2e41e97f
Yubo Zhang <clannad93@gmail.com>
2020-01-11 17:05:00 -0500

    MaximumSubarray.java

--- 53. Maximum Subarray/Java/MaximumSubarray.java
+++ 53. Maximum Subarray/Java/MaximumSubarray.java
@@ -1,14 +1,27 @@
 
 public class MaximumSubarray {
 	
-	public int maxSubArray(int[] nums) {
-		int[] dp = new int[nums.length];
-		int max = dp[0];
-		
-		dp[0] = nums[0];
+	public int maxSubArrayDP(int[] nums) {
+		int max = nums[0];
 		for(int i = 1; i < nums.length;i++){
-			dp[i] = dp[i-1]>=0?(dp[i-1]+nums[i]):nums[i];
-			max = Math.max(max, dp[i]);
+			nums[i] = nums[i-1]>=0?(nums[i-1]+nums[i]):nums[i];
+			max = Math.max(max, nums[i]);
+		}
+        return max;
+    }
+	
+	public int maxSubArrayGreedy(int[] nums) {
+		int max = Integer.MIN_VALUE;
+		int submax = Integer.MIN_VALUE;
+		
+		for (int n :nums){
+			if(submax<0){
+				submax = n;
+			}
+			else{
+				submax += n;
+			}
+			max = Math.max(max, submax);
 		}
         return max;
     }
