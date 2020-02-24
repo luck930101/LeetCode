00cae6c541980031ad6a1fd668dd2910d7703770
Yubo Zhang <clannad93@gmail.com>
2020-02-24 03:20:32 -0500

    Create SubarraySumEqualsK.java

---
+++ 560. Subarray Sum Equals K/Java/SubarraySumEqualsK.java
@@ -0,0 +1,22 @@
+
+public class SubarraySumEqualsK {
+    public int subarraySum(int[] nums, int k) {
+    	int count = 0;
+    	for (int i = 0; i < nums.length; i++) {
+			int sum = 0;
+			for (int j = i; j < nums.length; j++) {
+				sum+=nums[j];
+				if(sum == k ){
+					count++;
+				}
+			}
+		}
+
+    	return count;
+    }
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
