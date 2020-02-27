053d5e35687c1a37cdd12d272ab61e10bdffe58b
Yubo Zhang <clannad93@gmail.com>
2020-02-26 23:49:34 -0500

    Create BestTimetoBuyandSellStockII.java

---
+++ 122. Best Time to Buy and Sell Stock II/Java/BestTimetoBuyandSellStockII.java
@@ -0,0 +1,12 @@
+
+public class BestTimetoBuyandSellStockII {
+	public int maxProfit(int[] prices) {
+        int interest = 0;
+        for(int i = 1; i < prices.length;i++){
+        	if(prices[i]>prices[i-1]){
+        		interest+=prices[i]-prices[i-1];
+        	}
+        }
+        return interest;
+    }
+}
