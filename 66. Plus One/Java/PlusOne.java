22b0aaab9658ce93dec892ee64d1a0b88faeb9ac
Yubo Zhang <clannad93@gmail.com>
2020-02-27 00:12:57 -0500

    Create PlusOne.java

---
+++ 66. Plus One/Java/PlusOne.java
@@ -0,0 +1,26 @@
+
+public class PlusOne {
+	
+	public int[] plusOne(int[] digits) {
+		
+		for(int i = digits.length-1;i>=0;i--){
+			if(digits[i]<9){
+				digits[i]++;
+				return digits;
+			}
+			
+			digits[i] = 0;
+		}
+		
+		int[] result = new int[digits.length+1];
+		result[0]=1;
+		return result;
+		
+    }
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
