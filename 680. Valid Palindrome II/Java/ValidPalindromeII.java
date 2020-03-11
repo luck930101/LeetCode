d95ddedcc0975f0cbe1d7d8044e5e02897cc0edd
Yubo Zhang <clannad93@gmail.com>
2020-03-11 15:39:06 -0400

    Create ValidPalindromeII.java

---
+++ 680. Valid Palindrome II/Java/ValidPalindromeII.java
@@ -0,0 +1,26 @@
+
+public class ValidPalindromeII {
+	public boolean validPalindrome(String s) {
+        int low = 0;
+        int high = s.length()-1;
+        while(low <high){
+            if(s.charAt(low)!=s.charAt(high)){
+                return isPalindrome(s,low+1,high)||isPalindrome(s,low,high-1);
+            }
+            low++;
+            high--;
+        }
+        return true;
+        
+        
+    }
+    
+    public boolean isPalindrome(String s,int low, int high) {
+        while(low <high){
+            if(s.charAt(low)!=s.charAt(high)){return false;}
+            low++;
+            high--;
+        }
+        return true;
+    }
+}
