b2b7f00870464c956d75ad31d3ee6b782b0e94de
Yubo Zhang <clannad93@gmail.com>
2020-03-14 14:38:30 -0400

    Create Searcha2DMatrixII.java

---
+++ 240. Search a 2D Matrix II/Java/Searcha2DMatrixII.java
@@ -0,0 +1,20 @@
+
+public class Searcha2DMatrixII {
+	public boolean searchMatrix(int[][] matrix, int target) {
+        int r = matrix.length-1;
+        int c = 0;
+        
+        while(r >=0 &&c <matrix[0].length){
+            if(matrix[r][c] == target){
+                return true;
+            }
+            if(matrix[r][c] > target){
+                r--;
+            }
+            else{
+                c++;
+            }
+        }
+        return false;
+    }
+}
