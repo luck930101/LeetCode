1c7028bb0e7e212e8bc69f8fc642638bd9a27dab
Yubo Zhang <clannad93@gmail.com>
2020-03-26 00:55:39 -0400

    Create Pow.java

---
+++ 50. Pow(x, n)/Java/Pow.java
@@ -0,0 +1,16 @@
+
+public class Pow {
+	public double myPow(double x, int n) {
+        if(n==0){return 1;}
+        
+        if(n<0){
+            return 1/x*myPow(1/x,-(n+1));
+        }
+        if(n%2 == 0){
+            return myPow(x*x,n/2); 
+        }
+        else{
+            return x*myPow(x*x,n/2);
+        } 
+    }
+}
