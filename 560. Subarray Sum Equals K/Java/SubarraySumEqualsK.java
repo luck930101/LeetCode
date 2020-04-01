5466eca541ffb62f01d5febd453ed9dcd289c2fe
Yubo Zhang <clannad93@gmail.com>
2020-04-01 01:31:25 -0400

    Update SubarraySumEqualsK.java

--- 560. Subarray Sum Equals K/Java/SubarraySumEqualsK.java
+++ 560. Subarray Sum Equals K/Java/SubarraySumEqualsK.java
@@ -1,6 +1,9 @@
+import java.util.HashMap;
+import java.util.Map;
 
 public class SubarraySumEqualsK {
-    public int subarraySum(int[] nums, int k) {
+    public int subarraySumOn2(int[] nums, int k) {
+    	//O(n^2)
     	int count = 0;
     	for (int i = 0; i < nums.length; i++) {
 			int sum = 0;
@@ -14,9 +17,23 @@ public class SubarraySumEqualsK {
 
     	return count;
     }
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
+    
+    public int subarraySumOn(int[] nums, int k) {
+    	//O(n) 
+        Map<Integer,Integer> map = new HashMap<Integer,Integer>();
+        int sum = 0;
+        int count = 0;
+        map.put(0,1);
+        for(int n:nums){
+            sum+=n;
+            if(map.containsKey(sum-k)){
+                count+=map.get(sum-k);
+            }
+            
+            map.put(sum,map.getOrDefault(sum,0)+1);
+        }
+        
+        return count;
+    }
 
 }
