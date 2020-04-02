0137b8b5c1251efe0b193b935c2de7df328099e8
Yubo Zhang <clannad93@gmail.com>
2020-04-02 18:42:15 -0400

    Create MeetingRooms.java

---
+++ 252. Meeting Rooms/MeetingRooms.java
@@ -0,0 +1,14 @@
+import java.util.Arrays;
+
+public class MeetingRooms {
+	public boolean canAttendMeetings(int[][] intervals) {
+        Arrays.sort(intervals,(a,b)->a[0]==b[0]?a[1]-b[1]:a[0]-b[0]);
+        for(int i = 1;i<intervals.length;i++){
+            if(intervals[i][0]<intervals[i-1][1]){
+                return false;
+            }
+        }
+        
+        return true;
+    }
+}
