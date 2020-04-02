7553f6c1dca966f18c17df26148a25972d9c010e
Yubo Zhang <clannad93@gmail.com>
2020-04-02 01:39:15 -0400

    Create ValidateBinarySearchTree.java

---
+++ 98. Validate Binary Search Tree/Java/ValidateBinarySearchTree.java
@@ -0,0 +1,19 @@
+
+public class ValidateBinarySearchTree {
+	public boolean isValidBST(TreeNode root) {
+        return helper(root,null,null);
+    }
+    
+    public boolean helper(TreeNode root,Integer min,Integer max) {
+        if(root ==null){
+            return true;
+        }
+        if(min!=null&&root.val<=min){
+            return false;
+        }
+        if(max!=null&&root.val>=max){
+            return false;
+        }
+        return helper(root.left,min,root.val)&&helper(root.right,root.val,max);
+    }
+}
