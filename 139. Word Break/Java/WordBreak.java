d9c4ca27562f2674edc017c792219b7be1bcb608
Yubo Zhang <clannad93@gmail.com>
2020-04-11 18:22:19 -0400

    Create WordBreak.java

---
+++ 139. Word Break/Java/WordBreak.java
@@ -0,0 +1,19 @@
+import java.util.List;
+
+public class WordBreak {
+	public boolean wordBreak(String s, List<String> wordDict) {
+        boolean[] dp = new boolean[s.length()+1];
+        dp[0] = true;
+        
+        for(int i = 1; i< dp.length;i++){
+            for(int j = 0;j<i;j++){
+                if(dp[j] && wordDict.contains(s.substring(j,i))){
+                    dp[i] = true;
+                    break;
+                }
+                
+            }
+        }
+        return dp[dp.length-1];
+    }
+}
