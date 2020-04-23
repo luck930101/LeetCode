efd6364c6a8597c8ac7fea62bdc4de700d173acc
yz2586 <yz2586@cornell.edu>
2020-04-23 00:37:09 -0400

    Create LinkedListCycleII.java

---
+++ 142. Linked List Cycle II/Java/LinkedListCycleII.java
@@ -0,0 +1,27 @@
+
+public class LinkedListCycleII {
+	public ListNode detectCycle(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+        
+        while(fast!=null&&fast.next!=null){
+            slow = slow.next;
+            fast = fast.next.next;
+            if(slow ==fast){
+                break;
+            }
+        }
+        
+        if(fast==null||fast.next==null){
+            return null;
+        }
+        
+        fast = head;
+        
+        while(fast!=slow){
+            slow = slow.next;
+            fast = fast.next;
+        }
+        return slow;
+    }
+}
