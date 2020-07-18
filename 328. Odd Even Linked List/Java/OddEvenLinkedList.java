86137c9dbd0cb5ffef068e23560b7a6e98dcb546
Yubo Zhang <clannad93@gmail.com>
2020-07-18 12:41:48 -0700

    Update OddEvenLinkedList.java

--- 328. Odd Even Linked List/Java/OddEvenLinkedList.java
+++ 328. Odd Even Linked List/Java/OddEvenLinkedList.java
@@ -1,4 +1,22 @@
 
 public class OddEvenLinkedList {
-
+	public ListNode oddEvenList(ListNode head) {
+        if(head ==null ||head.next ==null){
+            return head;
+        }
+        ListNode odd = head;
+        ListNode even = head.next;
+        ListNode evenhead = even;
+        
+        while(even!=null&&even.next!=null){
+            odd.next = odd.next.next;
+            even.next = even.next.next;
+            odd = odd.next;
+            even = even.next;
+        }
+        
+        odd.next = evenhead;
+        
+        return head;
+    }
 }
